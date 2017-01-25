e0ed8e414fc3fd56d1e162b6f10005540a7418ba
Rutger Claes <rutger.claes@cegeka.be>
2017-01-25 19:46:41 +0100

    Used more types...

--- app/model/Raffle.scala
+++ app/model/Raffle.scala
@@ -10,12 +10,13 @@ sealed trait Raffle {
 
   def name:String
 
-  def people:Set[String]
+  def prizes:List[String]
 
 }
 
 object Raffle {
 
+  // winner name, price name
   type Outcome = (String,String)
 
   def empty( name:String, prizes:List[String] ) = EmptyRaffle( name, prizes )
@@ -24,33 +25,50 @@ object Raffle {
 
 case class EmptyRaffle( name:String, prizes:List[String] ) extends Raffle {
 
-  def addPerson( person:String ) = DraftRaffle( name, prizes, Set( person ) )
-
-  override def people = Set.empty
+  def addPerson( person:String ):RaffleWithPeople = DraftRaffle( name, prizes, Set( person ) )
 
 }
 
-case class DraftRaffle( name:String, prizes:List[String], people:Set[String] ) extends Raffle {
+trait RaffleWithPeople { this:Raffle =>
 
-  def addPerson( name:String ): Raffle = {
+  def people:Set[String]
+
+  def addPerson( name:String ): RaffleWithPeople = {
     val incPeople = people + name
     if (incPeople.size >= prizes.size)
       ReadyToRunRaffle(name, prizes, incPeople)
     else
-      copy(people = incPeople)
+      withPeople( incPeople )
   }
+
+  def withPeople( people:Set[String] ): RaffleWithPeople
+}
+
+case class DraftRaffle( name:String, prizes:List[String], people:Set[String] ) extends Raffle with RaffleWithPeople {
+
+  override def withPeople(people: Set[String]): RaffleWithPeople = copy( people = people )
 
 }
 
-case class ReadyToRunRaffle( name:String, prizes:List[String], people:Set[String] ) extends Raffle {
+case class ReadyToRunRaffle( name:String, prizes:List[String], people:Set[String] ) extends Raffle with RaffleWithPeople {
 
   require( prizes.size >= people.size )
 
-  def addPerson( name:String ) = copy( people = people + name )
+  override def withPeople(people: Set[String]): RaffleWithPeople = copy( people = people )
 
-  def run:(Outcome,Raffle) = ???
+  private def choosePerson:String = people.toList.head // FIXME
+
+  def run:RunningRaffle = RunningRaffle( name, prizes, people, List.empty )
 
   def finish:FinishedRaffle = ???
+
+}
+
+case class RunningRaffle( name:String, prizes:List[String], people:Set[String], outcome:List[Outcome] ) extends Raffle {
+
+  def leftOverPrices = ???
+
+  def run:RunningRaffle = ???
 
 }
 
