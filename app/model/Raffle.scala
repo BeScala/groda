d9e0053de42fcc8a0f181ef322b30019019526a9
Rutger Claes <rutger.claes@cegeka.be>
2017-01-25 22:12:31 +0100

    Make RaffleWithPeople play nice with tests.  fixes #5

--- app/model/Raffle.scala
+++ app/model/Raffle.scala
@@ -2,6 +2,8 @@ package model
 
 import model.Raffle.Outcome
 
+import scala.util.Random
+
 /**
   * Created by Rutger Claes <rutger.claes@cegeka.be> on 23/11/16.
   */
@@ -16,10 +18,8 @@ sealed trait Raffle {
 
 object Raffle {
 
-  // winner name, price name
+  // winner name, prize name
   type Outcome = (String,String)
-
-  def empty( name:String, prizes:List[String] ) = EmptyRaffle( name, prizes )
 
 }
 
@@ -33,43 +33,49 @@ trait RaffleWithPeople extends Raffle {
 
   def people:Set[String]
 
-  def addPerson( name:String ): RaffleWithPeople = {
+  def addPerson( name:String ): RaffleWithPeople
+
+}
+
+case class DraftRaffle private[model]( name:String, prizes:List[String], people:Set[String] ) extends RaffleWithPeople {
+
+  require( prizes.size > people.size )
+
+  override def addPerson(name: String): RaffleWithPeople = {
     val incPeople = people + name
-    if (incPeople.size >= prizes.size)
+    if (incPeople.size >= prizes.size )
       ReadyToRunRaffle(name, prizes, incPeople)
     else
-      withPeople( incPeople )
+      copy( people = incPeople )
+  }
+}
+
+case class ReadyToRunRaffle private[model]( name:String, prizes:List[String], people:Set[String] ) extends RaffleWithPeople {
+
+  require( prizes.size <= people.size )
+
+  override def addPerson(name: String): RaffleWithPeople = copy( people = people + name )
+
+  def finish:FinishedRaffle = {
+    def run( people:Set[String], prizes:List[String], outcome:List[Outcome]  ):FinishedRaffle =
+      prizes match {
+      case Nil => FinishedRaffle( name, outcome )
+      case _ => {
+        val winner:String = people.toList( Random.nextInt( people.size ) )
+        val prize = prizes.head
+
+        run( people - winner, prizes.tail, ( winner, prize ) :: outcome )
+      }
+
+    }
+
+    run( people, prizes, List.empty )
   }
 
-  def withPeople( people:Set[String] ): RaffleWithPeople
 }
 
-case class DraftRaffle( name:String, prizes:List[String], people:Set[String] ) extends RaffleWithPeople {
+case class FinishedRaffle private[model]( name:String, outcome:List[Outcome] ) extends Raffle {
 
-  override def withPeople(people: Set[String]): RaffleWithPeople = copy( people = people )
-
-}
-
-case class ReadyToRunRaffle( name:String, prizes:List[String], people:Set[String] ) extends RaffleWithPeople {
-
-  require( prizes.size >= people.size )
-
-  override def withPeople(people: Set[String]): RaffleWithPeople = copy( people = people )
-
-  private def choosePerson:String = people.toList.head // FIXME
-
-  def run:RunningRaffle = RunningRaffle( name, prizes, people, List.empty )
-
-  def finish:FinishedRaffle = ???
+  override def prizes: List[String] = List.empty
 
 }
-
-case class RunningRaffle( name:String, prizes:List[String], people:Set[String], outcome:List[Outcome] ) extends Raffle {
-
-  def leftOverPrices = ???
-
-  def run:RunningRaffle = ???
-
-}
-
-case class FinishedRaffle( name:String, prizes:List[String], people:Set[String], outcome:List[Outcome] ) extends Raffle
--- test/model/RaffleTest.scala
+++ test/model/RaffleTest.scala
@@ -7,15 +7,50 @@ import scala.util.Random
 /**
   * Created by Rutger Claes <rutger.claes@cegeka.be> on 23/11/16.
   */
-trait RaffleBehavior { this}
-
 class RaffleTest extends FlatSpec with Matchers {
 
-  def randomName = Random.nextString( 5 )
+  def randomName:String = Random.nextString( 5 )
 
-  def randomPrices = ( 1 to Random.nextInt( 10 ) + 1 ).map( i => Random.nextString( 5 ) ).toList
+  def randomName( size:Int ):List[String] = ( 0 until size ).map( _ => randomName ).toList
 
-  behavior of "EmptyRaffle"
+  def randomPrices = randomName( 1 + Random.nextInt( 10 ) )
+
+  behavior of "Raffle"
+
+  it should "not be runnable without sufficient people" in {
+    val empty = EmptyRaffle( randomName, randomName( 3 ) )
+
+    val one = empty.addPerson( randomName )
+    one shouldNot be( a[ReadyToRunRaffle] )
+    val two = one.addPerson( randomName )
+    two shouldNot be( a[ReadyToRunRaffle] )
+    val three = two.addPerson( randomName )
+
+    three shouldBe a[ReadyToRunRaffle]
+  }
+
+  it should "produce an outcome" in {
+    val raffle = EmptyRaffle( randomName, randomName( 3 ) )
+      .addPerson( randomName )
+      .addPerson( randomName )
+      .addPerson( randomName )
+      .addPerson( randomName )
+      .addPerson( randomName )
+      .addPerson( randomName )
+
+    val finished =
+      raffle match {
+        case r:ReadyToRunRaffle => r.finish
+        case _ => fail( "Should be ready to run" )
+      }
+
+    val (winners, prizes) = finished.outcome.unzip
+    winners.toSet.size shouldBe prizes.size
+    ( winners.toSet diff raffle.people ) shouldBe empty
+    prizes.sorted shouldBe raffle.prizes.sorted
+
+    finished.prizes shouldBe empty
+  }
 
   it should "be open to new people" in {
     val empty = EmptyRaffle(randomName, randomPrices)
@@ -46,45 +81,5 @@ class RaffleTest extends FlatSpec with Matchers {
 
     notEmpty.prizes shouldBe empty.prizes
   }
-
-  behavior of "DraftRaffle"
-
-  it should "not become ready when not full" in {
-    val draft = DraftRaffle( randomName, List( "a", "b", "c" ), Set( randomName ) )
-    val result = draft.addPerson( randomName )
-
-    result shouldBe a[DraftRaffle]
-  }
-
-  it should "actually add the person when not full" in {
-    val draft = DraftRaffle( randomName, List( "a", "b", "c" ), Set( randomName ) )
-    val secondName = randomName
-    val result = draft.addPerson( secondName )
-
-    result.people shouldBe draft.people + secondName
-  }
-
-  it should "preserve the name when not full" // TODO
-
-  it should "preserve the prizes when not full" // TODO
-
-  it should "become ready when full" in {
-    val draft = DraftRaffle( randomName, List( "a", "b" ), Set( randomName ) )
-    val result = draft.addPerson( randomName )
-
-    result shouldBe a[ReadyToRunRaffle]
-  }
-
-  it should "actually add the person when full" in {
-    val draft = DraftRaffle( randomName, List( "a", "b" ), Set( randomName ) )
-    val secondName = randomName
-    val result = draft.addPerson( secondName )
-
-    result.people shouldBe draft.people + secondName
-  }
-
-  it should "preserve the name when full" // TODO
-
-  it should "preserve the prizes when full" // TODO
 
 }
